e946ddc29d1e33d9f0a711cf5317f40ca1cdbd95
r3tr0tech2 <retrotechsolutions2@gmail.com>
2024-09-23 12:05:41 -0400

    Create rng.py

---
+++ rng.py
@@ -0,0 +1,9 @@
+# Generate a number from 0-255 to practice converting Decimal-Hex-Binary
+
+#import the random module
+
+import random
+# create the range using a for loop
+
+for i in range(10):
+        print(random.randint(0,255))
